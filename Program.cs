bd40ddac1664eb692706305188265b2dee838a39
gurigill07 <50706248+gurigill07@users.noreply.github.com>
2019-05-28 20:19:49 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PROG8170_Assignment1;
+using NUnit.Framework;
+
+namespace PROG8170_Assignment1_Test
+{
+    [TestFixture]
+    class PROG8170_Assignment1_Test
+    {
+        [Test]
+        public void GetLength_Input25_Returns25()
+        {
+
+            //Arrange
+            int length = 25;
+            int width = 5;
+
+            int expectedResult = length;
+
+            Rectangle testRectangle = new Rectangle(length, width);
+
+            //Act 
+            double actualResult = testRectangle.GetLength();
+
+            //Assert 
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        
+
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
